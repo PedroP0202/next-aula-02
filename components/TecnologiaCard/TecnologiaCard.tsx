43728da14155703760546b14d22ab69c97e42c97
PedroP0202 <160158774+PedroP0202@users.noreply.github.com>
2025-11-26 18:59:57 +0000

    Update TecnologiaCard.tsx

--- components/TecnologiaCard/TecnologiaCard.tsx
+++ components/TecnologiaCard/TecnologiaCard.tsx
@@ -6,12 +6,12 @@ interface TecnologiaCardProps {
   readonly description: string;
 }
 
-export default function TecnologiaCard(props : Readonly<TecnologiaCardProps>) {
+export default function TecnologiaCard(props : TecnologiaCardProps) {
   return (
-    <div className="w-40 h-40 bg-gray-100 p-4 m-2 rounded-lg flex flex-col items-center justify-center shadow-md hover:scale-105 transition-transform">
-     <h2>{props.title}</h2>
-     <h3>{props.image}</h3>
-      <h4 className=" text-black font-semibold">{props.description}</h4>
+    <div className="w-55 h-55 bg-gray-100 p-4 m-2 rounded-lg flex flex-col items-center justify-center shadow-md hover:scale-105 transition-transform">
+     <h2 className=" text-black"  >{props.title}</h2>
+     <h3 className="  text-black"  >{props.image}</h3>
+      <h4 className=" p-3 text-center text-black font-semibold">{props.description}</h4>
       
     </div>
   );
