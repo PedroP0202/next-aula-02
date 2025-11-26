5beb6be31bfcba51d201e8fe17582e429d558410
PedroP0202 <160158774+PedroP0202@users.noreply.github.com>
2025-11-26 22:49:39 +0000

    LAB10
    implementando o contador

--- app/Tecnologias/page.tsx
+++ app/Tecnologias/page.tsx
@@ -2,8 +2,24 @@
 import TecnologiaCard from "@/components/TecnologiaCard/TecnologiaCard";
 import tecnologias from "@/data/tecnologias.json";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function TecnologiasPage() {
+const [conta, setConta] = useState(() => {
+  const contaStorad = localStorage.getItem("conta")
+  return contaStorad ? Number(contaStorad) : 0
+})
+
+function aumentarConta(){
+  setConta(conta+1)
+}
+
+useEffect(()=>{
+    console.log("Conta ++", conta)
+    localStorage.setItem('conta',`${conta}`)
+},[conta])
+
+let coonta = 0
   return (
     <main className="p-6">
       <h2 className="text-2xl text-black font-semibold mb-4">Tecnologias</h2>
@@ -18,6 +34,15 @@ export default function TecnologiasPage() {
             />
           </Link>
         ))}
+        <p className="bla">Adicionando gostos: {conta}</p>
+        <button 
+        className="bg-black   p-3 hover:text-black hover:bg-white   rounded-xl"
+         onClick={() => setConta(conta+1)}>
+          Gosto++
+        </button>
+
+        
+
       </div>
     </main>
   );
