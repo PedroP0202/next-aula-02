bc9e3e626a062947a1fb755beb8a977d79cfbc8b
PedroP0202 <160158774+PedroP0202@users.noreply.github.com>
2025-11-28 14:21:29 +0000

    LAB10
    troca de cor dos textos , deixando os texto mais destacados

--- app/caracteristicas/page.tsx
+++ app/caracteristicas/page.tsx
@@ -14,7 +14,7 @@ const caracteristicas = [
 export default function CaracteristicasPage() {
   return (
     <main className="p-6">
-      <h1 className="text-2xl font-semibold mb-4">Características do React e do Next.js</h1>
+      <h1 className="text-black text-2xl font-semibold mb-4">Características do React e do Next.js</h1>
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {caracteristicas.map((c) => (
           <Caracteristica key={c} caracteristica={c} />
